eb331f70c03d1588c1feaa545bac798793e8ff37
Mat <62247805+MateuszKapelusz@users.noreply.github.com>
2022-04-18 20:59:58 +0200

    MainActivity Update + GoodHabitsApplication

--- app/src/main/java/com/example/goodhabitsapp/MainActivity.kt
+++ app/src/main/java/com/example/goodhabitsapp/MainActivity.kt
@@ -3,21 +3,40 @@ package com.example.goodhabitsapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.goodhabitsapp.navigation.SetupNavigation
 import com.example.goodhabitsapp.ui.theme.GoodHabitsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@ExperimentalAnimationApi
+@ExperimentalMaterialApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private lateinit var navController: NavHostController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GoodHabitsAppTheme {
-                // A surface container using the 'background' color from the theme
+                navController = rememberNavController()
+                SetupNavigation(
+                    navController = navController,
+
+                )
 
             }
         }
