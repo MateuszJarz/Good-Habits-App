2359059a5ecd6e2fd9be5bf1b69d7e5d1cf2e43c
Mat <62247805+MateuszKapelusz@users.noreply.github.com>
2022-04-18 21:59:46 +0200

    BottomMenuBar + Updates

---
+++ app/src/main/java/com/example/goodhabitsapp/screens/components/app_bars/BottomMenuBar.kt
@@ -0,0 +1,80 @@
+package com.example.goodhabitsapp.screens.components.app_bars
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.goodhabitsapp.R
+import com.example.goodhabitsapp.ui.theme.topAppBarBackgroundColor
+
+@Composable
+fun BottomMenuBar(
+    onClickedList: () -> Unit,
+    onClickedPomodoro: () -> Unit,
+    onClickedStatistic: () -> Unit,
+) {
+    BottomMenuBarComponent(
+        onClickedList = onClickedList,
+        onClickedTimer = onClickedPomodoro,
+        onClickedStatistic = onClickedStatistic,
+    )
+}
+
+@Composable
+fun BottomMenuBarComponent(
+    onClickedList: () -> Unit,
+    onClickedTimer: () -> Unit,
+    onClickedStatistic: () -> Unit,
+) {
+    BottomAppBar(
+        modifier = Modifier.fillMaxWidth(),
+        backgroundColor = MaterialTheme.colors.topAppBarBackgroundColor
+    ) {
+        IconButton(
+            modifier = Modifier.weight(1f),
+            onClick = {}
+        ) {
+            Icon(
+                imageVector = Icons.Filled.CheckCircle,
+                contentDescription = "Task Screen Icon"
+            )
+        }
+        IconButton(
+            modifier = Modifier.weight(1f),
+            onClick = {onClickedTimer()}
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_schedule),
+                contentDescription = "Pomodoro Screen Icon"
+            )
+        }
+        IconButton(
+            modifier = Modifier.weight(1f),
+            onClick = {}
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_bar_chart),
+                contentDescription = "Statistic Screen Icon"
+            )
+        }
+
+    }
+}
+
+@Preview
+@Composable
+fun BottomMenuBarPrev() {
+    BottomMenuBar(
+        onClickedList = {},
+        onClickedPomodoro = {},
+        onClickedStatistic = {}
+    )
+}
